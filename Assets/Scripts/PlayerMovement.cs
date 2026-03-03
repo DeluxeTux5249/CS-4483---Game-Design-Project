efd94e4991bddbdfa23c09a774a22600f7edaab6
DeluxeTux5249 <abrad.t.5249@gmail.com>
2026-03-03 16:44:53 -0500

    Finished Basic Player Movement
    Added animations - they are rough, and still need to account for movement direction, but they work.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,15 +10,35 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
 
     private Vector2 moveInput;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+        
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+
+        // determine if moving
+        if (moveInput == Vector2.zero)
+        {
+            animator.SetBool("isWalking", false);
+        } else
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        // determine direction
+
+
     }
 }
