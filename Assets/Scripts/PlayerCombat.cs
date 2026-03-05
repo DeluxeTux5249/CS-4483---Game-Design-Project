900652bd131ad8db49d3b3a428dc52d4375aa4fb
Joseph Liao <josephliao73@gmail.com>
2026-03-05 12:20:34 -0500

    add enemy health

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,6 +4,10 @@ public class PlayerCombat : MonoBehaviour
 {
     public Animator animator;
     public float cooldown = 1.5f;
+    public Transform attackPoint;
+    public float weaponRange = 1f;
+    public LayerMask enemyLayer;
+    public int damage = 1;
     private float timer;
 
     private void Update()
@@ -19,9 +23,20 @@ public class PlayerCombat : MonoBehaviour
         if(timer <= 0)
         {
             animator.SetBool("isAttacking", true);
+
             timer = cooldown;
         }
         animator.SetBool("isAttacking", true);
+    }
+
+    public void DealDamage()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
+        if (hitColliders.Length > 0)
+        {
+            hitColliders[0].GetComponent<EnemyHealth>().ChangeHealth(-damage);
+        }
+
     }
 
     public void finishAttack()
