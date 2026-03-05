4dd20d9b0a0132a4321c13fb1d03388514fa7bdd
DeluxeTux5249 <abrad.t.5249@gmail.com>
2026-03-05 15:29:36 -0500

    Healthbar Added
    Needs integration with enemy systems; strongly suggest merging the scripts for player & enemy health - no reason for those being separate.

--- Assets/Scripts/HealthTracker.cs
+++ Assets/Scripts/HealthTracker.cs
@@ -5,17 +5,26 @@ public class HealthTracker : MonoBehaviour
 
     [SerializeField] private int maxHealth = 10;
     [SerializeField] private int currentHealth = 10;
+    [SerializeField] private HealthBar healthBar;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentHealth = maxHealth; 
+        currentHealth = maxHealth;
+
+        if (healthBar != null )
+        {
+            healthBar.SetMaxHealth(maxHealth);
+            healthBar.SetHealth(currentHealth);
+        }
     }
 
     // returns true if creature dies, false otherwise.
     public bool GiveDamage(int dmgReceived)
     {
         currentHealth -= dmgReceived;
+        
+        if (healthBar != null) healthBar.SetHealth(currentHealth);
 
         // if you run out of health, you die.
         if (currentHealth <= 0) {
@@ -33,6 +42,8 @@ public class HealthTracker : MonoBehaviour
         {
             currentHealth = maxHealth;
         }
+
+        if (healthBar != null) healthBar.SetHealth(currentHealth);
     }
 
     // logic to resolve on death
