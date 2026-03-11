ef52bb423991631af9ac438a9c3105a73a38f459
Joseph Liao <josephliao73@gmail.com>
2026-03-11 13:10:06 -0400

    add enemy healthbar

--- Assets/Scripts/EnemyScripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyScripts/EnemyHealth.cs
@@ -1,20 +1,30 @@
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
+
 {
+    [SerializeField] private HealthBar healthBar;
     public int currentHealth;
     public int maxHealth;
     private void Start()
     {
         currentHealth = maxHealth;
-        
+
+        if (healthBar != null)
+        {
+            healthBar.SetMaxHealth(maxHealth);
+            healthBar.SetHealth(currentHealth);
+        }
+
     }
 
     public void ChangeHealth(int amount)
     {
+        if (healthBar != null) healthBar.SetHealth(currentHealth);
         currentHealth += amount;
         if (currentHealth > maxHealth) {
             currentHealth = maxHealth;
+            if (healthBar != null) healthBar.SetHealth(currentHealth);
         }
         else if (currentHealth <= 0)
         {
