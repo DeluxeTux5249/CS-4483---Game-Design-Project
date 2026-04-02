19c21f724e2d913c74440e2b4f3eb603250b195e
DeluxeTux5249 <abrad.t.5249@gmail.com>
2026-04-02 14:22:10 -0400

    Presentation alterations
    - Edited where dialogue interact button is
    - Created a dialogue area prefab for ease of creation & standardization
    - Added pop-up markers for dialogue

--- Assets/Scripts/DialogueManager/DialogueTrigger.cs
+++ Assets/Scripts/DialogueManager/DialogueTrigger.cs
@@ -12,9 +12,11 @@ public class DialogueTrigger : MonoBehaviour
     // could've probably done better by adding a supermanager, but this is already a stitching job, and it doesn't matter for now
     public DialogueTree tree;
     public Button interactButton;
+    public GameObject dialogue_pop_up;
 
     public void TriggerDialog()
     {
+        dialogue_pop_up.SetActive(false);
         FindAnyObjectByType<DialogueManager>().StartDialogue(tree.nodes[0]);       
     }
 
